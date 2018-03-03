d526b80dceb1d6007217ee3aca7462d410c35c3e
Jason Mosley <jpmos10@users.noreply.github.com>
2018-03-03 11:48:45 -0600

    added new viewas a separator and a new linearView for the visiting team

--- app/src/main/java/com/example/android/baseballscorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/baseballscorekeeper/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.android.baseballscorekeeper;
 
-import android.graphics.drawable.Drawable;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-import java.io.InputStream;
-import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,13 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-//    public static Drawable LoadImageFromWebOperations(String url) {
-//        try {
-//            InputStream is = (InputStream) new URL(url).getContent();
-//            Drawable d = Drawable.createFromStream(is, "src name");
-//            return d;
-//        } catch (Exception e) {
-//            return null;
-//        }
-//    }
+
+    public void addVisitorStrike(View view) {
+    }
 }

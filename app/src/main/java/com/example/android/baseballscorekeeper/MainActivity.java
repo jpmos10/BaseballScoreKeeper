a5e9d1eea27489a479b55d6929069357bcc55f3b
Jason Mosley <jpmos10@users.noreply.github.com>
2018-03-07 23:28:10 -0600

    modified MainActivity

--- app/src/main/java/com/example/android/baseballscorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/baseballscorekeeper/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.android.baseballscorekeeper;
 
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity
+    int
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

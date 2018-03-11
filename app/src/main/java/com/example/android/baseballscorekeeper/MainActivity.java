09323efd8f0e02f05fc188d756a7b5b689051ffb
Jason Mosley <jpmos10@users.noreply.github.com>
2018-03-10 22:10:33 -0600

    corrected visitor names that were copied from home teams code, extracted hardcoded sting values to strings.xml file

--- app/src/main/java/com/example/android/baseballscorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/baseballscorekeeper/MainActivity.java
@@ -8,8 +8,16 @@ import android.widget.TextView;
 
 
 
-public class MainActivity extends AppCompatActivity
-    int
+public class MainActivity extends AppCompatActivity{
+    int HomeStrike = 0;
+    int HomeBall = 0;
+    int HomeOut = 0;
+    int HomeScore = 0;
+    int VisitingStrike = 0;
+    int VisitingBall = 0;
+    int VisitingOut = 0;
+    int VisitingScore = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +25,34 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
     }
 
+    public void addHomeStrike(View view){
+            if (HomeStrike = 3) {
+                HomeOut = HomeOut + 1
+        }
+    public void addHomeBall(View view) {
+    }
 
-    public void addVisitorStrike(View view) {
+    public void addHomeOuts(View view) {
+    }
+    public void addHomeRunScored(View view) {
     }
 }
+
+    public void addVisitorStrike(View view) {
+        if ()
+    }
+
+    public void addVisitorRunScored(View view) {
+    }
+
+    public void addVisitorBall(View view) {
+    }
+
+    public void addVisitorOuts(View view) {
+    }
+
+    public void resetScore(View view) {
+    }
+
+
+
